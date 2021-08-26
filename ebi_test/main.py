d045a71a34bd70d9b9346c5e61ddc0a3f8f7cea5
donald-shrimp <t0706244@gmail.com>
2021-08-26 19:23:59 +0900

    やっとボタン出たわ

--- ebi_test/main.py
+++ ebi_test/main.py
@@ -1,154 +1,33 @@
 # coding: utf-8
-
 import discord
-import requests
-from discord.ext import commands
-from pprint import pprint
-import aiohttp
-TOKEN = 'token'
-#えび用テストボットのトークンなのでトークンの書き換えが必要です
+import random
+client = discord.Client()
 
-headers = {
-    "Authorization": "Bot AAAAA"
-}
+# ランダムで送るメッセージの一覧 ※ここに書き足すことでランダムに選ぶ内容を増やせる
+random_contents = [
+    "にゃーん",
+    "わん！",
+    "コケッコッコー",
+]
 
-
-def returnNormalUrl(channelId):
-    return "https://discordapp.com/api/channels/" + str(channelId) + "/messages"
-
-
-async def notify_callback(id, token):
-    url = "https://discord.com/api/v8/interactions/{0}/{1}/callback".format(id, token)
-    json = {
-        "type": 6
-    }
-    async with aiohttp.ClientSession() as s:
-        async with s.post(url, json=json) as r:
-            if 200 <= r.status < 300:
-                return
-
-
-def normalMessage(msg, content):
-    normal_url = returnNormalUrl(msg["d"]["channel_id"])
-    json = {
-        "content": content
-    }
-    r = requests.post(normal_url, headers=headers, json=json)
-
-
-async def on_socket_response(msg):
-    if msg["t"] != "INTERACTION_CREATE":
-        return
-
-    pprint(msg)
-    custom_id = msg["d"]["data"]["custom_id"]
-
-    if custom_id == "click_one":
-        normal_url2 = returnNormalUrl(msg["d"]["channel_id"]) + "/" + msg["d"]["message"]["id"]
-        json2 = {
-            "content": "LEFT",
-            "components": [
-                {
-                    "type": 1,
-                    "components": [
-                        {
-                            "type": 2,
-                            "label": "<",
-                            "style": 1,
-                            "custom_id": "click_one",
-                            "disabled": True
-                        },
-                        {
-                            "type": 2,
-                            "label": ">",
-                            "style": 3,
-                            "custom_id": "click_two",
-                            "disabled": False
-                        },
-                    ]
-                }
-            ]
-        }
-        r2 = requests.patch(normal_url2, headers=headers, json=json2)
-        pprint(r2)
-        await notify_callback(msg["d"]["id"], msg["d"]["token"])
-    elif custom_id == 'click_two':
-        normal_url2 = returnNormalUrl(msg["d"]["channel_id"]) + "/" + msg["d"]["message"]["id"]
-        json2 = {
-            "content": "Right",
-            "components": [
-                {
-                    "type": 1,
-                    "components": [
-                        {
-                            "type": 2,
-                            "label": "<",
-                            "style": 1,
-                            "custom_id": "click_one",
-                            "disabled": False
-                        },
-                        {
-                            "type": 2,
-                            "label": ">",
-                            "style": 3,
-                            "custom_id": "click_two",
-                            "disabled": True
-                        },
-                    ]
-
-                }
-            ]
-        }
-        r2 = requests.patch(normal_url2, headers=headers, json=json2)
-        pprint(r2)
-        await notify_callback(msg["d"]["id"], msg["d"]["token"])
-
-
-class MyBot(commands.Bot):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.add_listener(on_socket_response)
-
-
-bot = MyBot(command_prefix='$', description='slash test')
-
-
-@bot.event
+@client.event
 async def on_ready():
-    print("Boot")
-    await bot.change_presence(activity=discord.Game("プロセカ"))
+    print("on_ready")
+    print(discord.__version__)
 
 
-@bot.event
-async def on_message(msg):
-    if msg.content == "うんこ":
-        normal_url = returnNormalUrl(msg.channel.id)
-        json = {
-            "content": "LEFT",
-            "components": [
-                {
-                    "type": 1,
-                    "components": [
-                        {
-                            "type": 2,
-                            "label": "<",
-                            "style": 1,
-                            "custom_id": "click_one",
-                            "disabled": True
-                        },
-                        {
-                            "type": 2,
-                            "label": ">",
-                            "style": 3,
-                            "custom_id": "click_two"
-                        },
-                    ]
+@client.event
+async def on_message(message):
+    # 送信者がbotである場合は弾く
+    if message.author.bot:
+        return 
+    # メッセージの本文が 鳴いて だった場合
+    if message.content == "鳴いて":
+        # 送信するメッセージをランダムで決める
+        content = random.choice(random_contents)
+        # メッセージが送られてきたチャンネルに送る
+        await message.channel.send(content)
+    elif message.content == "おはよう":
+        await message.channel.send("おはよう！！")
 
-                }
-            ]
-        }
-        r = requests.post(normal_url, headers=headers, json=json)
-
-
-bot.run(TOKEN)
+client.run("token")
